0565e05bf01c7e1c655cba5d365ed2fa2dd7dc3b
kvin15 <vinayakkhamkar@gmail.com>
2018-05-03 13:25:27 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -8,3 +8,17 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(df):
+    X = df.drop(labels=['SalePrice'], axis=1)
+    y = df.SalePrice
+    rf = RandomForestClassifier(random_state=9)
+    rf.fit(X,y)
+    # prefit is set to True since we have already fitted a model
+    feat_selector = SelectFromModel(estimator=rf,prefit=True)
+    # This gives the array of all the features and selected feature indices are marked as True
+    feat_selector_arr = feat_selector.get_support()
+    # Get the indices which are marked as True from the above array
+    ind = np.where( feat_selector_arr == True )
+    # Select the indices from the feature columns passed to the selectfrommodel
+    sel_features = X.columns[ind].values.tolist()
+    return sel_features
